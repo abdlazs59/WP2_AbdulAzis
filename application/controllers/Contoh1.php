72f206bc9f19035000b5f92f9897df7faf869724
abdlazs <abdlazs2196@gmail.com>
2021-09-20 21:50:42 +0700

    Perkenalan

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan <h1>";
+        echo "Nama saya Abdul Azis 
+        Saya tinggal di daerah Senen Bermain Bola adalah Sepakbola";
+    }
+}
+
+
+
+
+?>
